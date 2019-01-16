738e5b03ceceb378add84863b0823fcf6fc9244c
Radom7 <929593448@qq.com>
2019-01-16 14:54:18 +0800

    111

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+//引入vant
+import Vant from 'vant';
+//引入全局的vant组件
+import 'vant/lib/index.css';
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
